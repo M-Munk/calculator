7ff9a33d573efe753606aeb2245b435d1752a2f4
BlueChihuahua <mmunk1994@gmail.com>
2020-08-26 03:17:24 -0600

    need to figure out how to input second number

--- assets/script.js
+++ assets/script.js
@@ -27,39 +27,82 @@ let displayValue = ""; //displays on screen
 const keys = document.querySelectorAll(".key");
 keys.forEach((key) => {
   key.addEventListener("click", (e) => {
-    console.log(e.srcElement.classList);
     if (e.srcElement.classList.contains("num")) {
-      console.log(e.srcElement.innerText);
+      if (displayValue === 0) {
+        displayValue = "";
+      }
       displayValue += e.srcElement.innerText;
-      document.getElementById("output").innerHTML = displayValue;
-      console.log(document.getElementById("output").innerHTML);
+      updateDisplay(displayValue);
+    }
+    if (e.srcElement.classList.contains("symbol")) {
+      operation = e.srcElement.innerText;
+      console.log(operation);
+      firstNumber = parseInt(displayValue);
+      console.log(firstNumber);
+      oneElement(firstNumber, operation);
+    }
+    if (firstNumber && operation) {
+      displayValue = "";
+      updateDisplay(displayValue);
+      //   if (e.srcElement.classList.contains("num")) {
+      //     displayValue += e.srcElement.innerText;
+      //     updateDisplay(displayValue);
+      //   } else if (e.srcElement.classList.contains("symbol")) {
+      //     secondNumber = parseInt(displayValue);
+      //     console.log(secondNumber);
+      //     result = calculate(firstNumber, secondNumber, operation);
+      //     updateDisplay(result);
+      //   }
     }
   });
 });
 
+function updateDisplay(displayValue) {
+  document.getElementById("output").innerHTML = displayValue;
+  return;
+}
+
+function oneElement(firstNumber, operation) {
+  if (operation === "AC") {
+    displayValue = 0;
+    updateDisplay(displayValue);
+    firstNumber = "";
+    return;
+  } else if (operation === "%") {
+    displayValue = percent(firstNumber);
+    updateDisplay(displayValue);
+    firstNumber = "";
+    return;
+  } else if (operation === "±") {
+    displayValue = changeSign(firstNumber);
+    updateDisplay(displayValue);
+    firstNumber = parseInt(displayValue);
+  }
+}
+
 function calculate(firstNumber, secondNumber, operation) {
   let result = 0;
   switch (operation) {
-    case "addition":
+    case "+":
       result = add(firstNumber, secondNumber);
       document.getElementById("output").innerHTML = result;
       return result;
-    case "subtraction":
+    case "-":
       result = subtract(firstNumber, secondNumber);
       document.getElementById("output").innerHTML = result;
       return result;
-    case "multiplication":
+    case "*":
       result = multiply(firstNumber, secondNumber);
       document.getElementById("output").innerHTML = result;
       return result;
-    case "division":
+    case "/":
       if (secondNumber === 0) {
         document.getElementById("output").innerHTML = "don't do that";
         break;
       }
       result = divide(firstNumber, secondNumber);
-      document.getElementById("output").innerHTML = result.toFixed(1);
-      return result.toFixed(1);
+      document.getElementById("output").innerHTML = result.toFixed(2);
+      return result.toFixed(2);
     default:
       break;
   }
