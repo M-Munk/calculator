8164483cdd3234ec48315acfbd994fef46e34c5f
BlueChihuahua <mmunk1994@gmail.com>
2020-08-31 23:58:29 -0600

    functionality working, need to add decimal point

--- assets/script.js
+++ assets/script.js
@@ -18,78 +18,123 @@ function percent(a) {
   return a / 100;
 }
 //variables for calculator operation
-let firstNumber = ""; //stores first value before operator
-let secondNumber = ""; //stores second value after operator
-let operation = ""; //stores operator
+let firstNumber = null; //stores first value before operator
+let secondNumber = null; //stores second value after operator
+let operation = null; //stores operator
 let displayValue = ""; //displays on screen
-let result = "";
 
-//listen for clicks on keys
-const keys = document.querySelectorAll(".key");
-keys.forEach((key) => {
-  key.addEventListener("click", (e) => {
-    if (e.srcElement.classList.contains("num") && !firstNumber) {
-      if (displayValue === 0) {
-        displayValue = "";
+//listen for clicks on numbers
+const nums = document.querySelectorAll(".num");
+nums.forEach((num) => {
+  num.addEventListener("click", (e) => {
+    console.log(e.srcElement.innerText);
+    let numClicked = e.srcElement.innerText;
+    setDisplayValue(numClicked);
+  });
+});
+
+// listen for clicks on symbols and do math:
+// if there is no first number set,
+// sets the first number and operation when operation is clicked
+// then sets operation in continuing cases after it is cleared by updateVariables
+// ie after single value operation or after multiple operations where
+// result becomes the first operand
+// last it will set the second operand and if the final operator is not "=", it will
+// set the next operator in sequence
+
+const symbols = document.querySelectorAll(".symbol");
+symbols.forEach((symbol) => {
+  symbol.addEventListener("click", (e) => {
+    console.log(e.srcElement.innerText);
+    let mathSymbol = e.srcElement.innerText;
+    if (!firstNumber) {
+      if (mathSymbol !== "=") {
+        setOperation(mathSymbol);
       }
-      displayValue += e.srcElement.innerText;
-      updateDisplay(displayValue);
-    }
-    if (e.srcElement.classList.contains("symbol")) {
-      operation = e.srcElement.innerText;
-      firstNumber = parseInt(displayValue);
+      setFirstNumber(displayValue);
+      displayValue = 0;
       oneElement(firstNumber, operation);
-    }
-    if (firstNumber && operation) {
-      if (e.srcElement.classList.contains("num")) {
-        displayValue += e.srcElement.innerText;
-        updateDisplay(displayValue);
+    } else if (firstNumber && !operation) {
+      if (mathSymbol !== "=") {
+        setOperation(mathSymbol);
       }
-    }
-    if (
-      e.srcElement.classList.contains("symbol") &&
-      !secondNumber &&
-      firstNumber &&
-      operation &&
-      displayValue
-    ) {
-      secondNumber = parseInt(displayValue);
+      console.log(operation);
+      displayValue = 0;
+      oneElement(firstNumber, operation);
+    } else if (!secondNumber) {
+      setSecondNumber(displayValue);
       console.log(secondNumber);
-      result = calculate(firstNumber, secondNumber, operation);
+      let result = calculate(firstNumber, secondNumber, operation);
       updateDisplay(result);
-      if (e.srcElement.innerText === "=") {
-        // clearVariables();
-      } else firstNumber = result;
+      if (mathSymbol !== operation && mathSymbol !== "=") {
+        setOperation(mathSymbol);
+        displayValue = 0;
+      }
     }
   });
 });
 
-//updates the display value
-function updateDisplay(displayValue) {
+//updates the display value & makes sure it won't overrun display
+function updateDisplay(value) {
+  string = value.toString();
+  if (string.length > 12) {
+    console.log("true");
+    displayValue = value.toExponential(4);
+    console.log(displayValue);
+  }
   document.getElementById("output").innerHTML = displayValue;
   return;
 }
 
+function setDisplayValue(numberClicked) {
+  if (displayValue.length >= 12) {
+    return;
+  }
+  if (displayValue === 0) {
+    displayValue = "";
+  }
+  displayValue += numberClicked;
+  updateDisplay(displayValue);
+}
+
+//variable setters
+function setFirstNumber(displayNum) {
+  firstNumber = parseFloat(displayNum);
+}
+
+function setOperation(clickedSymbol) {
+  operation = clickedSymbol;
+}
+
+function setSecondNumber(displayNum) {
+  secondNumber = parseFloat(displayNum);
+  return secondNumber;
+}
+
 //evaluates single argument calculator operations
 function oneElement(firstNum, math) {
-  if (math === "AC") {
-    displayValue = 0;
-    clearVariables();
-    return;
-  } else if (math === "%") {
-    displayValue = percent(firstNum);
-    updateDisplay(displayValue);
-    operation = "";
-    firstNumber = parseInt(displayValue);
-    return;
-  } else if (math === "±") {
-    displayValue = changeSign(firstNum);
-    updateDisplay(displayValue);
-    operation = "";
-    firstNumber = parseInt(displayValue);
-  } else if (math === "+" || math === "-" || math === "*" || math === "/") {
-    displayValue = "";
-    updateDisplay(displayValue);
+  let result = 0;
+  switch (math) {
+    case "AC":
+      clearVariables(0);
+      updateDisplay(displayValue);
+      return;
+    case "%":
+      result = percent(firstNum);
+      displayValue = result;
+      updateDisplay(displayValue);
+      firstNumber = parseFloat(displayValue);
+      operation = null;
+      return;
+    case "±":
+      result = changeSign(firstNum);
+      displayValue = result;
+      updateDisplay(displayValue);
+      firstNumber = parseFloat(displayValue);
+      operation = null;
+      return;
+    default:
+      return;
   }
 }
 
@@ -99,36 +144,44 @@ function calculate(firstNum, secondNum, math) {
   switch (math) {
     case "+":
       result = add(firstNum, secondNum);
-      document.getElementById("output").innerHTML = result;
-      clearVariables();
+      displayValue = result;
+      updateVariables();
       return result;
     case "-":
       result = subtract(firstNum, secondNum);
-      document.getElementById("output").innerHTML = result;
-      clearVariables();
+      displayValue = result;
+      updateVariables();
       return result;
     case "*":
       result = multiply(firstNum, secondNum);
-      document.getElementById("output").innerHTML = result;
-      clearVariables();
+      displayValue = result;
+      updateVariables();
       return result;
-    case "/":
+    case "÷":
       if (secondNum === 0) {
         document.getElementById("output").innerHTML = "don't do that";
-        break;
+        return;
       }
       result = divide(firstNum, secondNum);
-      document.getElementById("output").innerHTML = result.toFixed(2);
-      clearVariables();
-      return result.toFixed(2);
+      displayValue = result;
+      updateVariables();
+      return result;
     default:
       break;
   }
 }
 
 //clears all math variables
-function clearVariables() {
-  firstNumber = "";
-  secondNumber = "";
-  operation = "";
+function clearVariables(displayResult) {
+  firstNumber = null;
+  secondNumber = null;
+  operation = null;
+  displayValue = displayResult;
+}
+
+//updates variables after operation & sets first number to the result
+function updateVariables() {
+  secondNumber = null;
+  firstNumber = displayValue;
+  operation = null;
 }
